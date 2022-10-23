4a92921c402eb6545f1338808c12769cc2e1f231
Mayowa <mayowa320@gmail.com>
2022-10-23 08:20:21 -0400

    fixed other issues preventing build

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -37,41 +37,41 @@ export const ADD_USER = gql`
     }
   }
 `;
-export const SAVE_BOOK = (gql = `
-    mutation Mutation($input: BookInput) {
-        saveBook(input: $input) {
-            _id
-            email
-            username
-            password
-            bookCount
-            savedBooks {
-                bookId
-                authors
-                description
-                title
-                image
-                link
-            }
-        }
+export const SAVE_BOOK = gql`
+  mutation Mutation($input: BookInput) {
+    saveBook(input: $input) {
+      _id
+      email
+      username
+      password
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
     }
-`);
-export const REMOVE_BOOK = (gql = `
-    mutation Mutation($bookId: String) {
-        removeBook(bookId: $bookId) {
-            _id
-            email
-            username
-            password
-            bookCount
-            savedBooks {
-                bookId
-                authors
-                description
-                title
-                image
-                link
-            }
-        }
+  }
+`;
+export const REMOVE_BOOK = gql`
+  mutation Mutation($bookId: String) {
+    removeBook(bookId: $bookId) {
+      _id
+      email
+      username
+      password
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
     }
-`);
+  }
+`;
--- server/server.js
+++ server/server.js
@@ -8,8 +8,8 @@ const cors = require("cors");
 const { json } = require("body-parser");
 
 const { ApolloServer } = require("@apollo/server");
-import { startStandaloneServer } from "@apollo/server/standalone";
-const { authMiddleware } = require("../utils/auth");
+const { startStandaloneServer } = require("@apollo/server/standalone");
+const { authMiddleware } = require("./utils/auth");
 
 const { expressMiddleware } = require("@apollo/server/express4");
 
@@ -23,24 +23,24 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-startStandaloneServer(server, {
-  // Note: This example uses the `req` argument to access headers,
-  // but the arguments received by `context` vary by integration.
-  // This means they vary for Express, Fastify, Lambda, etc.
+//startStandaloneServer(server, {
+// Note: This example uses the `req` argument to access headers,
+// but the arguments received by `context` vary by integration.
+// This means they vary for Express, Fastify, Lambda, etc.
 
-  // For `startStandaloneServer`, the `req` and `res` objects are
-  // `http.IncomingMessage` and `http.ServerResponse` types.
-  context: authMiddleware,
-}).then(() => {
-  server.start().then(() => {
-    app.use("/graphql", cors(), json(), expressMiddleware(server));
-  });
+// For `startStandaloneServer`, the `req` and `res` objects are
+// `http.IncomingMessage` and `http.ServerResponse` types.
+//context: authMiddleware,
+//}).then(() => {
+server.start().then(() => {
+  app.use("/graphql", cors(), json(), expressMiddleware(server));
 });
+//});
 
 // // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-}
+//if (process.env.NODE_ENV === "production") {
+app.use(express.static(path.join(__dirname, "../client/build")));
+//}
 
 // app.use(routes);
 
